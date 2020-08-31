50d98d73da003ead322993040a720b82e0e3667f
narine <narinemkn@gmail.com>
2020-08-31 13:38:08 +0200

    Updates

---
+++ precache-manifest.3c9331595dcad7f991bd9a29d920b59a.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7891ae48f46fd758f9db4f70eb0edf1",
+    "url": "/Marketing-Agency/index.html"
+  },
+  {
+    "revision": "0c2bfa2166801d6821de",
+    "url": "/Marketing-Agency/static/css/2.5a9a206f.chunk.css"
+  },
+  {
+    "revision": "791b0c89be1c1ed0ccd4",
+    "url": "/Marketing-Agency/static/css/main.476223f6.chunk.css"
+  },
+  {
+    "revision": "0c2bfa2166801d6821de",
+    "url": "/Marketing-Agency/static/js/2.ac15edf5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Marketing-Agency/static/js/2.ac15edf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "791b0c89be1c1ed0ccd4",
+    "url": "/Marketing-Agency/static/js/main.4907e03b.chunk.js"
+  },
+  {
+    "revision": "4c7f33ed4a65e3ff5d3e",
+    "url": "/Marketing-Agency/static/js/runtime-main.30a1cacd.js"
+  },
+  {
+    "revision": "27481223ebe1d8dcd6a523cd12b329dc",
+    "url": "/Marketing-Agency/static/media/HP-about.27481223.png"
+  },
+  {
+    "revision": "f358799aa2356cbd934c05d6c6b0bb57",
+    "url": "/Marketing-Agency/static/media/HP-services.f358799a.png"
+  },
+  {
+    "revision": "838dc1a6c40bbaba4853a1b3d20e0179",
+    "url": "/Marketing-Agency/static/media/HP-testimonials.838dc1a6.png"
+  },
+  {
+    "revision": "14fbf139f0f40cd5dcefc760700eda15",
+    "url": "/Marketing-Agency/static/media/MOJO-header.14fbf139.png"
+  },
+  {
+    "revision": "c8547bd8ace7b2c29c4a76d9dc0b4c6d",
+    "url": "/Marketing-Agency/static/media/MOJO-nav.c8547bd8.png"
+  },
+  {
+    "revision": "d484b40e14a1096b568f370c0d5002dd",
+    "url": "/Marketing-Agency/static/media/MicrosoftTeams-image.d484b40e.jpeg"
+  },
+  {
+    "revision": "76740e8f2752d68cf0e7b6cbe74b41b5",
+    "url": "/Marketing-Agency/static/media/UI.76740e8f.jpg"
+  },
+  {
+    "revision": "1bafdd29828f087bed1f48fadf83a145",
+    "url": "/Marketing-Agency/static/media/aboutHeader.1bafdd29.png"
+  },
+  {
+    "revision": "0c2c406407bda5a99bd4dc1fb0481148",
+    "url": "/Marketing-Agency/static/media/brand.0c2c4064.jpg"
+  },
+  {
+    "revision": "91b133379eb49c2f1b6bdddd44494cf3",
+    "url": "/Marketing-Agency/static/media/call.91b13337.jpg"
+  },
+  {
+    "revision": "bb4e995e5136b751b45d2b625dc0e414",
+    "url": "/Marketing-Agency/static/media/development.bb4e995e.jpg"
+  },
+  {
+    "revision": "91c710b74a88ad5db966975e67c2204e",
+    "url": "/Marketing-Agency/static/media/eenlogo.91c710b7.png"
+  },
+  {
+    "revision": "71c6f4a6d26e596deb0ba50b21c9b8b4",
+    "url": "/Marketing-Agency/static/media/facebook.71c6f4a6.jpg"
+  },
+  {
+    "revision": "166fd46930154eccba830f986153e991",
+    "url": "/Marketing-Agency/static/media/gif.166fd469.png"
+  },
+  {
+    "revision": "de3394acb681962336795c740c4faf37",
+    "url": "/Marketing-Agency/static/media/insta.de3394ac.jpg"
+  },
+  {
+    "revision": "89710530fecc2349f7c21f53c795914a",
+    "url": "/Marketing-Agency/static/media/jobsHeader.89710530.png"
+  },
+  {
+    "revision": "ec94b675e0e58c095b4f8dbfd8229035",
+    "url": "/Marketing-Agency/static/media/marketing.ec94b675.jpg"
+  },
+  {
+    "revision": "221f91be11f33b7a43dc4d4f55491224",
+    "url": "/Marketing-Agency/static/media/me1.221f91be.jpg"
+  },
+  {
+    "revision": "e564589d177ac619545abd44de72f4c0",
+    "url": "/Marketing-Agency/static/media/mojovid.e564589d.mp4"
+  },
+  {
+    "revision": "3fb3fe7b55ef30f7723637df97fe5dfb",
+    "url": "/Marketing-Agency/static/media/next.3fb3fe7b.jpg"
+  },
+  {
+    "revision": "54764e2f3af1d17eed8eba09cc6f15c6",
+    "url": "/Marketing-Agency/static/media/prev.54764e2f.jpg"
+  },
+  {
+    "revision": "7bd04f3b161794521c88aa4112aaddaa",
+    "url": "/Marketing-Agency/static/media/profielfoto.7bd04f3b.jpg"
+  },
+  {
+    "revision": "1b6af61ced9970105ebd360bc4b9f9ee",
+    "url": "/Marketing-Agency/static/media/socialmedia.1b6af61c.jpg"
+  },
+  {
+    "revision": "004ff8af1f9ef2cfa289e80a970c94ab",
+    "url": "/Marketing-Agency/static/media/strategy.004ff8af.jpg"
+  },
+  {
+    "revision": "be93875e845db2e3acc8b7e7909b3685",
+    "url": "/Marketing-Agency/static/media/strategy.be93875e.jpg"
+  },
+  {
+    "revision": "55aa4d9e218ac9fff6ca82939c0c1846",
+    "url": "/Marketing-Agency/static/media/webdev.55aa4d9e.jpg"
+  }
+]);
